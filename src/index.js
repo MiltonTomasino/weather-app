f0408d1dfd226ae5c3bc6bb8560d91a1ede9f55b
MiltonTomasino <jonathantomasino13@gmail.com>
2025-05-11 17:32:16 -0700

    Create Basic Search Function and Location Display
    The user will be able to input a location and see their chosen location
    displayed at the top of the grid box.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./style.css";
 console.log("hello from index.js!");
 
 const key = "?key=B2H2UQPZ5CHBGDQH52BCFXUPA"
@@ -9,7 +10,7 @@ const WeatherApi = (() => {
 
     async function fetchCityData(city = null) {
         try {
-            let response = await fetch(url + "/San_Francisco" + key);
+            let response = await fetch(url + `/${city}` + key);
             cityData = await response.json();
             console.log(cityData);
 
@@ -22,9 +23,54 @@ const WeatherApi = (() => {
         return cityData.currentConditions.temp;
     }
 
+    function fetchCityDays() {
+        return cityData.days;
+    }
+
+    function fetchCity() {
+        return cityData;
+    }
+
     return {
         fetchCityData,
         fetchCurrentTemp,
+        fetchCityDays,
+        fetchCity
     }
+})();
+
+
+const DisplayInfo = (() => {
+    let searchBtn = document.querySelector("#search-btn");
+    let locationDiv = document.querySelector(".location");
+
+    searchBtn.addEventListener("click", async (e) => {
+        e.preventDefault();
+        locationDiv.innerHTML = "";
+        let search = document.querySelector("#search");
+        let formattedSearch = formatSearch(search.value);
+        await WeatherApi.fetchCityData(formattedSearch);
+        search.value = "";
+        displayLocation();
+    })
+
+    function formatSearch(term) {
+        let res = term.trim();
+        
+        if (res.includes(" ")) {
+            res = res.split(" ").join("_");
+        }
+
+        return res;
+    }
+
+    async function displayLocation() {
+        let locationH1 = document.createElement("h1");
+        let cityLocation = WeatherApi.fetchCity();
+
+        locationH1.textContent = cityLocation.resolvedAddress;
+        locationDiv.appendChild(locationH1);
+    }
+
 })();
 
